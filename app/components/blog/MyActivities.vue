a82062bcf9cc3845551c0a66b3a0b231bb761eb6
azelalynetan <azelalynetan@gmail.com>
2025-10-05 19:33:34 +0800

    Add blog page

---
+++ app/components/blog/MyActivities.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="mb-24">
+    <article class="mb-6">
+      <header class="text-center sm:text-left">
+        <h2 class="text-xl text-gray-700 font-bold mb-2">My activities ✨</h2>
+        <p class="text-gray-700">Stories and experiences from my tech journey — the things I do outside of code.</p>
+      </header>
+    </article>
+  </section>
+</template>
